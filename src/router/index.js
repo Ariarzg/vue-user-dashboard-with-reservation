d7a9769c9f027fa1aff1544c77d73fe96de56a6c
Aria Rouzegar <ariarzg@gmail.com>
2025-08-09 03:13:51 +0330

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import LoginPage from '@/views/LoginPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{ path: '/', component: LoginPage }],
+});
+
+export default router;
